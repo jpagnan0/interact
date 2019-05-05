ee1752f4af4877e22c2bc8b4cdaf459cbdb370ed
jpagnan0 <a.jpagnano@gmail.com>
2019-05-05 12:23:57 -0400

    Initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
   # before_action :authorized
+  protect_from_forgery with: :exception
+
   def encode_token(user_id)
     JWT.encode(user_id, ENV["hmac_secret"])
   end
